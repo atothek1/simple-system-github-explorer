a291d8df53227d5ae94116abf562a8d96bc1244d
atothek1 <atothek1@gmail.com>
2024-03-19 12:23:09 +0100

    exporting ui components from index file

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { Box } from "./Box";
+export { List } from "./List";
+export { SimplePagination } from "./SimplePagination";
+export { Text } from "./Text";
+export { TextInput } from "./TextInput";
