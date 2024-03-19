a7be43cd0911e268acc62216143bf5b60c039372
atothek1 <atothek1@gmail.com>
2024-03-19 12:53:39 +0100

    smaller polishing

--- src/components/project/SearchResult/index.tsx
+++ src/components/project/SearchResult/index.tsx
@@ -3,8 +3,17 @@ import { Text } from "../../ui/Text";
 import { FetchResult } from "./FetchResult";
 
 export function SearchResult(){
+    const str = `"${ searchTerm.value }"`;
+    
+    if( searchTerm.value === "" ) {
+        return null;
+    }
+
     return ( <>
-        <Text as="h1">Search Result</Text>
+        <Text as="h1">
+            Search Result for 
+            { str }
+        </Text>
         { searchTerm.value !== "" ? <FetchResult searchTerm={ searchTerm.value } /> : null }
     </> );
 }
