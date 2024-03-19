1fa369a6217b34a30637195b0f67481fc932b229
atothek1 <atothek1@gmail.com>
2024-03-19 12:21:00 +0100

    added repo data model

--- src/api/types.ts
+++ src/api/types.ts
@@ -9,3 +9,16 @@ export interface SearchUsersData {
     readonly total: number;
     readonly items: User[];
 }
+
+export interface Repository {
+    readonly id: number;
+    readonly name: string;
+    readonly fullName: string;
+    readonly description: string;
+    readonly stars: number;
+    readonly url: string;
+}
+export interface UserRepositoriesData {
+    readonly total: number;
+    readonly items: Repository[];
+}
