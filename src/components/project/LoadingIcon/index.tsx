ff8b753b1c4fc9437f19f0ddb68849874b5fecb5
atothek1 <atothek1@gmail.com>
2024-03-22 10:32:17 +0100

    added title element to LoadingIcon

--- src/components/project/LoadingIcon/index.tsx
+++ src/components/project/LoadingIcon/index.tsx
@@ -1,6 +1,7 @@
 export function LoadingIcon() {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="64px">
+            <title>Loading animation</title>
             <circle fill="#2C9DDB" stroke="#2C9DDB" strokeWidth="15" r="15" cx="40" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></circle>
             <circle fill="#2C9DDB" stroke="#2C9DDB" strokeWidth="15" r="15" cx="100" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></circle>
             <circle fill="#2C9DDB" stroke="#2C9DDB" strokeWidth="15" r="15" cx="160" cy="100"><animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></circle>
