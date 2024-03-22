e0bc7b6e64ca423c27e125628605f85f6d07414c
atothek1 <atothek1@gmail.com>
2024-03-22 10:32:41 +0100

    added a fetch polyfill for testing fetch data

--- tests/setup.js
+++ tests/setup.js
@@ -2,6 +2,7 @@ import { expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import "cross-fetch/polyfill";
 
 expect.extend( matchers );
 
