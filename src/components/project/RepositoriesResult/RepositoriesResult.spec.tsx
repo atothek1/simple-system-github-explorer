4ebbb6a16fd99092fd5addd96e00da3ba7dd17f5
atothek1 <atothek1@gmail.com>
2024-03-22 10:34:10 +0100

    added basic integration tests for SearchResult and RepositoriesResult components

---
+++ src/components/project/RepositoriesResult/RepositoriesResult.spec.tsx
@@ -0,0 +1,71 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+
+import responseData1 from "../../../../public/data/repositories-1.json";
+import responseData2 from "../../../../public/data/repositories-2.json";
+
+import { RepositoriesResult } from ".";
+
+const handlers = [
+    http.get( /\/data\/repositories-1\.json/i, () => {
+        return HttpResponse.json( responseData1 ); } 
+    ),
+    http.get( /\/data\/repositories-2\.json/i, () => {
+        return HttpResponse.json( responseData2 ); } 
+    )
+];
+const server = setupServer( ...handlers );
+
+beforeAll( () => server.listen() );
+afterEach( () => server.resetHandlers() );
+afterAll( () => server.close() );
+
+describe( "RepositoriesResult", () => {
+    const name = "test";
+    it( "should render loading svg.", async () => {
+        const { queryByText } = render( <RepositoriesResult name={ name } /> );
+
+        expect( queryByText( "Loading animation" ) ).toBeInTheDocument();
+    } );
+
+    it( "should render list of repositories", async () => {
+        const { findByRole } = render( <RepositoriesResult name={ name } /> );
+        const element = await findByRole( "list" );
+
+        expect( element ).toBeInTheDocument();
+        expect( element.children.length ).toBe( 5 );
+    } );
+
+    // NOTE: a to big test case, should be splitted to seperated test, just want to showcase testing possibilities
+    it( "should fetch next page and render result", async () => {
+        const { findByRole } = render( <RepositoriesResult name={ name } /> );
+        const firstPage = await findByRole( "list" );
+
+        expect( firstPage ).toBeInTheDocument();
+        expect( firstPage.children.length ).toBe( 5 );
+
+        let nextBtn = screen.getByText( "Next" );
+        let prevBtn = screen.getByText( "Previous" );
+
+        expect( nextBtn ).toBeEnabled();
+        expect( prevBtn ).toBeDisabled();
+
+        await userEvent.click( nextBtn );
+
+        nextBtn = screen.getByText( "Next" );
+        prevBtn = screen.getByText( "Previous" );
+
+        expect( nextBtn ).toBeDisabled();
+        expect( prevBtn ).toBeEnabled();
+
+        const secondPage = await findByRole( "list" );
+
+        expect( secondPage ).toBeInTheDocument();
+        expect( secondPage.children.length ).toBe( 4 );
+
+    } );
+} );
---
+++ src/components/project/SearchResult/SearchResult.spec.tsx
@@ -0,0 +1,45 @@
+import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/react";
+
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+
+import responseData from "../../../../public/data/repositories-1.json";
+import { SearchResult } from ".";
+import { searchTerm } from "../../../state/search";
+
+const handlers = [
+    http.get( /\/data\/usersSearch\.json/i, () => {
+        return HttpResponse.json( responseData ); } 
+    )
+];
+const server = setupServer( ...handlers );
+
+beforeAll( () => server.listen() );
+afterEach( () => server.resetHandlers() );
+afterAll( () => server.close() );
+
+describe( "SearchResult", () => {
+    it( "should render empty dom element.", async () => {
+        const { container } = render( <SearchResult /> );
+
+        expect( container ).toBeEmptyDOMElement();
+    } );
+
+    it( "should render heading", async () => {
+        searchTerm.value = "test";
+        const { getByRole } = render( <SearchResult /> );
+        const headingValue = "Search Result for\"test\"";
+
+        expect( getByRole( "heading" ) ).toHaveTextContent( headingValue );
+    } );
+
+    it( "should render list of users", async () => {
+        searchTerm.value = "test";
+        const { findByRole } = render( <SearchResult /> );
+        const element = await findByRole( "list" );
+
+        expect( element ).toBeInTheDocument();
+        expect( element.children.length ).toBe( 5 );
+    } );
+} );
